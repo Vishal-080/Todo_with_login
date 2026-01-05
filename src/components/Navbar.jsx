a2b98d2a4e184bad3f00c1ee528f491697a2a18d
Vishal Rathod <vrathod591@gmail.com>
2026-01-06 00:19:34 +0530

    Daisy Ui navbar component added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-300 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">My Todo App</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Login</a>
+          </li>
+          <li>
+            <a>Sign-up</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

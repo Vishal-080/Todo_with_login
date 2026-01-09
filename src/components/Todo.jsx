c68f67c2ff8000259ec92334712e7869cdbbc5d1
Vishal Rathod <vrathod591@gmail.com>
2026-01-09 23:59:37 +0530

    Todo adding genAI

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,22 @@
 import React, { useState } from 'react'
+import { GoogleGenAI } from "@google/genai";
 
 const Todo = () => {
 
   const [todo, setTodo] = useState("");
+  
+
+const ai = new GoogleGenAI({});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how to learn everything about AI, i'm frontend developer with 6month of experience",
+  });
+  console.log(response.text);
+}
+
+await main();
 
   return (
     <div><input type="text" placeholder="Type here" className="input" />
